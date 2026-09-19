8896bc22a00049df515d1794b5ebcefa9472ddf0
shahmeer-codes <ransahahmeerarshad@gmail.com>
2026-09-19 12:05:28 +0500

    write in a file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        StreamWriter w = new StreamWriter(@"C:\Users\Shahmeer\Desktop\data.txt");
+        w.Write("Hello World");
+        w.Close();
+    }
+}
